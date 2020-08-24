d6dbb26b152354f740919d3ec7700f5a60858163
davey108 <sligar.david@gmail.com>
2020-08-24 00:56:28 -0400

    Added files for starting

---
+++ src/speechparser.py
@@ -0,0 +1,10 @@
+
+"""
+Module to transform an audio file with sounds to text string using SpeechRecognition package and 
+its two classes: Recognizer and AudioFile.
+"""
+
+class SpeechToText:
+    def __init__(self, audio_file):
+        print("This is the constructor for: ", self.__class__)
+    
